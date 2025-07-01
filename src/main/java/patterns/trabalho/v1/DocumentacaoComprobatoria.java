3212561b2e3d6d41c238029164b05b25c413ca69
willianbolzan <willian.bolzan@ifsc.edu.br>
2025-06-30 21:06:39 -0300

    dfdf

--- src/main/java/patterns/trabalho/v1/DocumentacaoComprobatoria.java
+++
@@ -1,9 +0,0 @@
-package patterns.trabalho.v1;
-
-public class DocumentacaoComprobatoria {
-    private String descricao;
-
-    public DocumentacaoComprobatoria(String descricao) {
-        this.descricao = descricao;
-    }
-}
--- src/main/java/patterns/trabalho/v1/ItemMenu.java
+++
@@ -1,7 +0,0 @@
-package patterns.trabalho.v1;
-
-public interface ItemMenu {
-    String numero();
-    String titulo();
-    void exibir();
-}
